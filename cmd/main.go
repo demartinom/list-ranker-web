12017f158e62e7bcfa7d2e468a207a29c1d264e2
Matt DeMartino <matt.s.demartino@gmail.com>
2024-11-04 12:42:36 -0500

    Create test api route to send message

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -9,9 +10,20 @@ func serverStart(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Server started")
 }
 
+type Message struct {
+	Text string `json:"text"`
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	data := Message{Text: "Hello from the backend"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
 func main() {
 	fmt.Println("Starting server on port 8080")
 
 	http.HandleFunc("/", serverStart)
+	http.HandleFunc("/message", test)
 	http.ListenAndServe(":8080", nil)
 }
