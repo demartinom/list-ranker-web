c0c85999c2c7bdf9163236e77b21acd77aaf6e05
Matt DeMartino <matt.s.demartino@gmail.com>
2024-11-04 13:13:46 -0500

    Create middleware to add cors headers to all routes

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,23 @@ type Message struct {
 	Text string `json:"text"`
 }
 
+// Set headers for all handleFuncs to enable CORS
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Handle preflight (OPTIONS) request
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func test(w http.ResponseWriter, r *http.Request) {
 	data := Message{Text: "Hello from the backend"}
 	w.Header().Set("Content-Type", "application/json")
