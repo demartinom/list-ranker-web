f10a5f51d43e15fa53fb91a5004dd74f6c255cdf
Matt DeMartino <matt.s.demartino@gmail.com>
2024-11-08 12:46:55 -0500

    Move Websocket to separate file

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
 // // Set headers for all handleFuncs to enable CORS
@@ -24,33 +22,6 @@ import (
 // 		next.ServeHTTP(w, r)
 // 	})
 // }
-
-// Create instance of Upgrader struct to upgrade http connection to websocket
-var upgrader = websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024, CheckOrigin: func(r *http.Request) bool { return true }}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer ws.Close()
-
-	log.Println("Connected Successfully")
-
-	for {
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			log.Printf("Error reading message %v\n", err)
-		}
-		log.Printf("Received message : %s\n", msg)
-
-		if err = ws.WriteMessage(websocket.TextMessage, msg); err != nil {
-			log.Printf("Error: %v\n", err)
-			break
-		}
-	}
-}
 
 func main() {
 	http.HandleFunc("/ws", handleConnections)
