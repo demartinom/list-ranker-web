35b06ea24c0317a73dc9821cf8d96493bd8a236a
Matt DeMartino <matt.s.demartino@gmail.com>
2024-11-14 14:32:32 -0500

    Create separate structs for list type

--- pkg/battle/types.go
+++ pkg/battle/types.go
@@ -16,3 +16,11 @@ type ReceivedMessage struct {
 	MessageType string          `json:"messageType"`
 	Data        json.RawMessage `json:"data"`
 }
+
+type PremadeList struct {
+	List string
+}
+
+type CustomList struct {
+	List []string
+}
