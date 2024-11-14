bd703217f29d0e0fd9d75cda63eb0e945b10f4ca
Matt DeMartino <matt.s.demartino@gmail.com>
2024-11-14 11:27:46 -0500

    Create input for user to create custom list and convert to array

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,13 @@
+import CustomList from "./customList";
 import WebSocketComponent from "./websocket";
 
 function App() {
-  return <WebSocketComponent />;
+  return (
+    <>
+      <WebSocketComponent />
+      <CustomList />
+    </>
+  );
 }
 
 export default App;
