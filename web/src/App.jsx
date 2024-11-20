bbaf8150840077b038b073cc3d7badf27db5d89e
Matt DeMartino <matt.s.demartino@gmail.com>
2024-11-20 15:17:06 -0500

    Remove socket init from app.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from "react";
 import ListSelection from "./ListSelection";
-import initWebSocket from "./websocket";
 
 function App() {
-  const [socket, setSocket] = useState(null);
-  useEffect(() => {
-    setSocket(initWebSocket());
-
-    return () => {
-      if (socket) {
-        socket.close();
-      }
-    };
-  }, []);
   return (
     <>
-      <ListSelection socket={socket} />
+      <ListSelection />
     </>
   );
 }
